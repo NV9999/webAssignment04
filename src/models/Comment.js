55769a8e4c74e9c4c05f8124c5ed22e9773effb7
Ninad <10viranininad@gmail.com>
2024-04-03 20:43:55 -0400

    Cart and Comment model done

---
+++ src/models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    image: String,
+    text: {
+        type: String,
+        required: true
+    }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
